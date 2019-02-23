b0075dce4bebb32f82762bc1db82d317b4c918b3
utsushiiro <ozeki.haru@gmail.com>
2019-02-24 02:23:46 +0900

    add password column to user

--- server/models/user.js
+++ server/models/user.js
@@ -3,7 +3,27 @@ module.exports = (sequelize, DataTypes) => {
   const user = sequelize.define(
     "user",
     {
-      name: DataTypes.STRING
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
     },
     {
       underscored: true
