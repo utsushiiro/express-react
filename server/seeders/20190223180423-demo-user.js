7283eb5524bec096a9674b375ef2e8d84152aea5
utsushiiro <ozeki.haru@gmail.com>
2019-02-24 03:16:39 +0900

    create seed for user

---
+++ server/seeders/20190223180423-demo-user.js
@@ -0,0 +1,22 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          name: "test",
+          password: "test",
+          created_at: new Date(),
+          updated_at: new Date()
+        }
+      ],
+      {}
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("Users", null, {});
+  }
+};
